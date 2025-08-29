6bdbd417d255fb7e76cd4978ccd4e85d973787fb
xsip <xsip@pm.me>
2025-08-29 15:54:36 +0200

    Init

---
+++ Example/Include.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+#pragma comment(lib, "Dumper.lib")
+namespace CS2 {
+	using GetOffsetFunc = DWORD(*)(std::string);
+	__declspec(dllimport)  GetOffsetFunc SetupCSchemaSystem(bool log);
+}
