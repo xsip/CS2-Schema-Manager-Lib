edf5218da6a8cb0b17bc6596557da20edeeba114
xsip <xsip@pm.me>
2025-08-31 18:12:45 +0200

    Adds comment to header

--- Example/Include.h
+++ Example/Include.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <Windows.h>
 #include <string>
-#pragma comment(lib, "Dumper.lib")
 namespace CS2 {
 	using GetOffsetFunc = DWORD(*)(std::string);
+	// Game dir is optional! If not provided, source2gen's steam game path resolver will be used!
+	// if provided make sure its the root game dir, not any bin dir! for example: 
+	// "D:\SteamLibrary\steamapps\common\Counter-Strike Global Offensive"
 	__declspec(dllimport)  GetOffsetFunc SetupCSchemaSystem(bool log, std::string gameDir = {});
 }
