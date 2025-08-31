8f134f2a3648c23e17595e17214d17e21fd0b1fd
xsip <xsip@pm.me>
2025-08-31 18:11:02 +0200

    Updates dumper

--- Example/Include.h
+++ Example/Include.h
@@ -4,5 +4,5 @@
 #pragma comment(lib, "Dumper.lib")
 namespace CS2 {
 	using GetOffsetFunc = DWORD(*)(std::string);
-	__declspec(dllimport)  GetOffsetFunc SetupCSchemaSystem(bool log);
+	__declspec(dllimport)  GetOffsetFunc SetupCSchemaSystem(bool log, std::string gameDir = {});
 }
