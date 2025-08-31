c9164da6db186444c7047719db945ec9d7303097
xsip <xsip@pm.me>
2025-08-31 22:18:11 +0200

    Adds new options to dumper

--- Example/Include.h
+++ Example/Include.h
@@ -1,10 +1,41 @@
 #pragma once
 #include <Windows.h>
 #include <string>
+#include <map >
+
+namespace SDK {
+	class CSchemaManagerField {
+	public:
+		DWORD GetOffset();
+		const char* GetType();
+	};
+
+	class CSchemaManagerClass {
+	public:
+		CSchemaManagerField* GetField(std::string fieldName);
+		std::map<const char*, CSchemaManagerField*> GetAllFields();
+	};
+
+	class CSchemaManagerModule {
+	public:
+		CSchemaManagerClass* GetClass(std::string className);
+		std::map<const char*, CSchemaManagerClass*> GetAllClasses();
+	};
+
+}
+
 namespace CS2 {
-	using GetOffsetFunc = DWORD(*)(std::string);
+
+	class Dumper {
+	public:
+		DWORD GetOffsetFast(std::string szSchemaPath);
+		SDK::CSchemaManagerModule* GetModule(std::string moduleName);
+		std::map <const char*, SDK::CSchemaManagerModule*> GetAllModules();
+
+	};
+
 	// Game dir is optional! If not provided, source2gen's steam game path resolver will be used!
 	// if provided make sure its the root game dir, not any bin dir! for example: 
 	// "D:\SteamLibrary\steamapps\common\Counter-Strike Global Offensive"
-	__declspec(dllimport)  GetOffsetFunc SetupCSchemaSystem(bool log, std::string gameDir = {});
+	__declspec(dllimport)  Dumper* SetupCSchemaSystem(bool log, std::string gameDir = {});
 }
--- Example/Main.cpp
+++ Example/Main.cpp
@@ -1,6 +1,22 @@
 #include "Include.h"
 
 int main() {
-	auto GetOffset = CS2::SetupCSchemaSystem(true);
-	printf("m_iHealth: 0x%x\n", GetOffset("client->C_BaseEntity->m_iHealth"));
+	auto dumper = CS2::SetupCSchemaSystem(true);
+	// Gettings offsets fast by selector
+	printf("m_iHealth: 0x%x\n", dumper->GetOffsetFast("client->C_BaseEntity->m_iHealth"));
+
+	// mapping through "client.dll" class list
+	auto classList = dumper->GetModule("client")->GetAllClasses();
+	for (const auto classData : classList) {
+		if (strcmp(classData.first, "C_BaseEntity") == 0) {
+			printf("Class: %s\n", classData.first);
+
+			auto fieldList = classData.second->GetAllFields();
+			for (const auto fieldData : fieldList) {
+				printf("\tField: %s ( 0x%x )\n", fieldData.first, fieldData.second->GetOffset());
+			}
+		}
+	}
 }
+
+// 
